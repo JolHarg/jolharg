245e15755ded1f329c2cffda902a3a67602953be
Dan Dart <git@dandart.co.uk>
2017-12-19 20:41:37 +0000

    css builder

---
+++ build.sh
@@ -0,0 +1,10 @@
+PATH=$PWD/node_modules/.bin:$PATH
+
+echo Building root
+uncss -H . -s /node_modules/bootstrap/dist/css/bootstrap.css,/css/index.css index.html >> css/external.css
+for DIR in $(find js/components/* -type d)
+do
+    [ -f $DIR/index.html ] || continue;
+    echo Building $DIR
+    uncss -H . -s /node_modules/bootstrap/dist/css/bootstrap.css,/css/index.css $DIR/index.html >> $DIR/external.css
+done
