4c5f2ff514f077833f11c15d9f358baab46083ab
Dan Dart <git@dandart.co.uk>
2018-01-03 17:57:48 +0000

    transpilation for old browsers - needs polyfills

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ do
     echo Building $DIR
     uncss -H . -s /node_modules/bootstrap/dist/css/bootstrap.css,/css/index.css $DIR/index.html >> $DIR/external.css
 done
+babel -d js-old js
+browserify js-old/index.js -o bundle.js
+rm -rf js-old
