53c9ff006be145787ea60b3d61dd03943474dbd0
Dan Dart <git@dandart.co.uk>
2018-04-28 06:02:23 +0100

    eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     plugins: [
         new webpack.EnvironmentPlugin(
             [
-                'GITHUB_ACCESS_TOKEN'
+                `GITHUB_ACCESS_TOKEN`
             ]
         ),
         new HtmlWebpackPlugin(
