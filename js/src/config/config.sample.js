02d1870cc25e97a8eb3b5cc71eb8e4c2f45dc653
Dan Dart <git@dandart.co.uk>
2018-04-28 04:46:57 +0100

    fix gitignore

---
+++ js/src/config/config.sample.js
@@ -0,0 +1,3 @@
+export const github = {
+    access_token: `abc`
+};
---
+++ js/src/lib/get-github-repos.js
@@ -0,0 +1,32 @@
+import { github } from '../config/config.js';
+import { fetchjson } from './ajax.js';
+
+const qs = `per_page=100&sort=pushed&type=owner&direction=desc&access_token=${github.access_token}`;
+
+export default async user => {
+    const arrResponse = await fetchjson(
+        `https://api.github.com/users/${user}/repos?${qs}`
+    );
+
+    if (arrResponse.message) {
+        throw new Error(arrResponse.message);
+    }
+
+    return arrResponse.map(item => ({
+        picture: item.owner.avatar_url,
+        name: item.name,
+        stars: item.stargazers_count ?
+            ` (${item.stargazers_count}&starf;)` :
+            ``,
+        fork: item.fork ?
+            ` (fork)`:
+            ``,
+        description: item.description ?
+            item.description :
+            `(no description)`,
+        licence: item.license && `Other` !== item.license.name ?
+            `<br/><a href="https://spdx.org/licenses/${item.license.spdx_id}.html" target="_blank">${item.license.spdx_id}</a>`:
+            ``,
+        url: item.clone_url
+    }));
+};
